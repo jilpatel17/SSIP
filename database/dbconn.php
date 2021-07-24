1e5fb338fce823c08ccc0c6311deee28bab87cfe
jil patel <jil1704.jp@gmail.com>
2021-07-24 16:23:06 +0530

    remote database commit

--- database/dbconn.php
+++ database/dbconn.php
@@ -1,5 +1,9 @@
 <?php
+<<<<<<< HEAD
     $conn = mysqli_connect("remotemysql.com","Zf2eceTt9a","xJSVwKBzun","Zf2eceTt9a");
+=======
+    $conn = mysqli_connect("localhost","root","","ssip");
+>>>>>>> 551be6c6fe35d45ccc67608a28e3f8f266c5b52c
     $from = 'jil1710.jp@gmail.com';
     if(!$conn)
     {
