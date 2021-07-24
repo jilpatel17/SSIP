c120736c9f9312fd8919b0278a48a4e2c57e7727
Ronak Adroja <84452183+ronakadroja@users.noreply.github.com>
2021-07-25 00:51:10 +0530

    first commit

---
+++ database/dbconn.php
@@ -0,0 +1,9 @@
+<?php
+    
+    $conn = mysqli_connect("remotemysql.com","Zf2eceTt9a","xJSVwKBzun","Zf2eceTt9a");
+    $from = 'jil1710.jp@gmail.com';
+    if(!$conn)
+    {
+        die("Connection to server is not established !");
+    }
+?>
